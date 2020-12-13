7fb29d5cc535039005425cafe0801420151b781b
devonekenobi <devonekenobi@gmail.com>
2020-12-12 22:03:36 -0600

    added SQL and Storage scripts

---
+++ Azure SQL/CreateAzureSQLDB.ps1
@@ -0,0 +1,51 @@
+﻿$dbServerName = <yourDBServerName>
+$dbName = <yourDBName>
+
+#create database server
+az sql server create \
+--admin-user <your-admin-user> \
+--admin-password <your-admin-pw> \
+-n $dbServerName \
+-g resource-group-west \
+-l westus \
+--enable-public-network true \
+--verbose
+
+#create firewall rule for public access
+az sql server firewall-rule create \
+-g resource-group-west \
+-s $dbServerName \
+-n azureaccess \
+--start-ip-address 0.0.0.0 \
+--end-ip-address 0.0.0.0 \
+--verbose
+
+#create firewall rule for your pc access to the db
+az sql server firewall-rule create \
+-g resource-group-west \
+-s $dbServerName \
+-n clientip \
+--start-ip-address <your-pc-ip-address> \
+--end-ip-address 255.255.255.0 \
+--verbose
+
+#create the database
+az sql db create \
+-n hello-udacity-db \
+-g resource-group-west \
+-s $dbServerName \
+--tier Basic \
+--verbose
+
+#delete the database
+az sql db delete \
+--n $dbName \
+--g resource-group-west \
+--s $dbServerName \
+--verbose
+
+#delete the server
+az sql server delete \
+--n $dbServerName \
+--g resource-group-west \
+--verbose
---
+++ Azure Storage Container/CreateAzureStorage.ps1
@@ -0,0 +1,14 @@
+﻿$storageName = <yourStorageName>
+
+#create storage account
+az storage account create \
+ -n $storageName \
+ -g resource-group-west \
+ -l westus
+
+#create storage container
+az storage container create \
+ --account-name $storageName \
+ -n images \
+ --auth-mode login \
+ --public-access container
